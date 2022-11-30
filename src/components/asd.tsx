bf9440e4808cd722c571d259eb896ed46bbc1d64
Дмитрий Горбатенко <dm.gorbatenko@gmail.com>
2022-12-01 00:49:56 +0200

    router test

---
+++ src/components/asd.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from "react";
+
+interface AsdProps {
+}
+
+type Props = AsdProps;
+
+const Asd: FC<Props> = ({}) => {
+    return (
+        <div>
+            asdasdasd page!
+        </div>
+    );
+};
+
+export default Asd;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Asd from "./components/asd";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route index element={<App />}/>
+              <Route path='/asd' element={<Asd/>}/>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 )
